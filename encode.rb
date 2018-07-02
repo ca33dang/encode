1df9441c69afb96ac81dfe21ae87577a6dd5032e
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-07-02 12:24:54 -0700

    got encode to pass

---
+++ encode.rb
@@ -0,0 +1,6 @@
+def roman_encode(msg)
+	{msg => msg.tr("A-Za-z", "F-ZA-Ef-za-e")}
+end
+def roman_decode(msg)
+	{msg => msg.tr("F-ZA-Ef-za-e", "A-Za-z")}
+end
---
+++ test_encode.rb
@@ -0,0 +1,26 @@
+require "minitest/autorun"
+require_relative "encode.rb"
+class TestTest < Minitest::Test
+
+	def test_assert_1_equals_1
+		assert_equal(1, 1)
+	end
+
+	def test_encode_returns_sting
+		assert_equal(Hash, roman_encode("").class)
+	end
+	def test_encode_shifts_by_5
+		assert_equal({"a" => "f"}, roman_encode("a"))
+	end
+	def test_encode_shifts_2_letters_by_5
+		assert_equal({"ab" => "fg"}, roman_encode("ab"))
+	end
+	def test_decode_unshifts
+	    assert_equal({"fg" => "ab"}, roman_decode("fg")) 
+	end
+	def test_complex_encode_decode
+		some_message = "kill"
+		assert_equal({"kill" => "pnqq"}, roman_encode(some_message))
+		assert_equal({"pnqq" => "kill"}, roman_decode("pnqq"))
+	end
+end
